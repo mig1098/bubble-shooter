907fcd16fe485667abec68a06f70a407144ec151
Marla Brizel <marlabrizel@gmail.com>
2015-10-28 17:52:09 -0600

    fix test failures

--- test/bubble_test.js
+++ test/bubble_test.js
@@ -22,59 +22,56 @@ describe('Bubble', function() {
     });
 
     it('can move left', function() {
-      let shooter = new Bubble(225, 440, 10);
+      let shooter = new Bubble(235, 440, 10);
       shooter.moveLeft();
 
-      assert.equal(shooter.x, 220);
+      assert.equal(shooter.x, 213);
       assert.equal(shooter.y, 440);
     });
 
     it('can move right', function() {
-      let shooter = new Bubble(225, 440, 10);
+      let shooter = new Bubble(235, 440, 10);
       shooter.moveRight();
 
-      assert.equal(shooter.x, 230);
+      assert.equal(shooter.x, 257);
       assert.equal(shooter.y, 440);
     });
 
     it('can move back and forth', function() {
-      let shooter = new Bubble(225, 440, 10);
+      let shooter = new Bubble(235, 440, 10);
       shooter.moveLeft().moveLeft().moveRight();
 
-      assert.equal(shooter.x, 220);
+      assert.equal(shooter.x, 213);
       assert.equal(shooter.y, 440);
     });
 
     it('cannot move past the left boundary', function() {
-      let shooter = new Bubble(15, 440, 10);
+      let shooter = new Bubble(40, 440, 10);
       shooter.moveLeft();
-      assert.equal(shooter.x, 10);
-
-      shooter.moveLeft();
-      assert.equal(shooter.x, 10);
+      assert.equal(shooter.x, 18);
     });
 
     it('can move right if on the left boundary', function() {
-      let shooter = new Bubble(10, 440, 10);
+      let shooter = new Bubble(20, 440, 10);
       shooter.moveRight();
 
-      assert.equal(shooter.x, 15);
+      assert.equal(shooter.x, 42);
     });
 
     it('cannot move past the right boundary', function() {
-      let shooter = new Bubble(435, 440, 10);
+      let shooter = new Bubble(411, 440, 10);
       shooter.moveRight();
-      assert.equal(shooter.x, 440);
+      assert.equal(shooter.x, 433);
 
       shooter.moveRight();
-      assert.equal(shooter.x, 440);
+      assert.equal(shooter.x, 433);
     });
 
     it('can move left if on the right boundary', function() {
-      let shooter = new Bubble(440, 440, 10);
+      let shooter = new Bubble(433, 440, 10);
       shooter.moveLeft();
 
-      assert.equal(shooter.x, 435);
+      assert.equal(shooter.x, 411);
     });
 
     it('shoots the ball when the spacebar is pressed', function() {
